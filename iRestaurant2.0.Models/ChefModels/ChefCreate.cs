95decac26ed2ebe289d02cad67943d7e1726b0ea
Dustin Pritsch <dlpritsch@gmail.com>
2020-06-05 10:51:38 -0400

    adding chef

---
+++ iRestaurant2.0.Models/ChefModels/ChefCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRestaurant2._0.Models.ChefModels
+{
+    public class ChefCreate
+    {
+        public string Full_Name { get; set; }
+        public string Speciality { get; set; }
+    }
+}
---
+++ iRestaurant2.0.Models/ChefModels/ChefDetail.cs
@@ -0,0 +1,17 @@
+﻿using iRestaurant2._0.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRestaurant2._0.Models.ChefModels
+{
+    public class ChefDetail
+    {
+        public int ChefID { get; set; }
+        public string Full_Name { get; set; }
+        public string Speciality { get; set; }
+        public Signature_Dish SignatureDish { get; set; }
+    }
+}
